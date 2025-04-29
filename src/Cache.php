90dfa5aebb4c47daf34e80a3611785f9d518ec61
Aidan Casey <aidan@caseyhouse.net>
2025-04-29 11:09:23 -0400

    refactor: package structure

---
+++ src/Cache.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Cache;
+
+use Closure;
+use Psr\Cache\CacheItemInterface;
+use Tempest\DateTime\DateTimeInterface;
+
+interface Cache
+{
+    public function put(string $key, mixed $value, ?DateTimeInterface $expiresAt = null): CacheItemInterface;
+
+    public function get(string $key): mixed;
+
+    /** @param Closure(): mixed $cache */
+    public function resolve(string $key, Closure $cache, ?DateTimeInterface $expiresAt = null): mixed;
+
+    public function remove(string $key): void;
+
+    public function clear(): void;
+
+    public function isEnabled(): bool;
+}
